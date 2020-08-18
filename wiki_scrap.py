5a1cc72cd7a16b20b0400c89ccd4c0de612f08a4
Lucas Simoes <38574936+akabame@users.noreply.github.com>
2020-08-18 17:23:07 -0300

    ultima versão sem set

--- wiki_scrap.py
+++ wiki_scrap.py
@@ -6,8 +6,10 @@ from selenium.webdriver.common.by import By
 from datetime import datetime
 import numpy as np
 import os
+import time
 #import re
-dir_path = os.path.dirname(__file__)
+#dir_path = os.path.dirname(__file__)
+dir_path = 'C:\\Users\\Lucas\\Desktop\\bullshitices'
 #escreve info das paginas em txt
 def escreve_txt(path,vetor,tipo):
     with open(path,tipo, encoding="utf-16") as outfile:
@@ -20,7 +22,7 @@ def webScrapper(url):
     global driver
     
     driver.get("{}".format(url))
-#    now = datetime.now()
+    #    now = datetime.now()
     source = driver.page_source
     html = soup(source, 'html.parser')
     
@@ -40,26 +42,30 @@ def webScrapper(url):
                 vet_hyper.append(str(a['href'])+'\n')
         vet_txt.append(str(i.text))
     
-    escreve_txt("{}\\{}.txt".format(dir_path,nome),vet_txt,'w')
-    escreve_txt("{}\\{}_hyper.txt".format(dir_path,nome),vet_hyper,'w')
+    escreve_txt("{}\\scrap_text\\{}.txt".format(dir_path,nome),vet_txt,'w')
+    escreve_txt("{}\\scrap_hyper\\{}_hyper.txt".format(dir_path,nome),vet_hyper,'w')
     escreve_txt("{}\\todas_urls.txt".format(dir_path,nome),vet_hyper,'a')
 #    print(datetime.now()-now)
     return vet_hyper
 
 #oções para navegador chrome
 opts = Options()
-#opts.add_argument("--headless")
+opts.add_argument("--headless")
+opts.add_argument("--log-level=3")
 #opts.add_argument("--disable-gpu")
 
 #chrome driver para navegar
-chr_d = '{}\\chromedriver.exe'.format(dir_path)
+#chr_d = '{}\\chromedriver.exe'.format(dir_path)
+chr_d = 'C:\\Users\\Lucas\\Desktop\\bullshitices\\chromedriver.exe'
+
 # driver = webdriver.Chrome(executable_path = chr_d, opts)
 driver = webdriver.Chrome(options = opts,executable_path = chr_d)
 
 #inicialização
 try:
     urls_lidas = open("{}\\ja_lidos.txt".format(dir_path),"r")
-    ja_lidos = urls_lidas.readlines() 
+    ja_lidos = urls_lidas.readlines()
+#    ja_lidos = set(ja_lidos)
     urls_lidas.close()
 #    print("!")
 except:
@@ -72,20 +78,25 @@ if ja_lidos == 0:
     novo_hyper = webScrapper(url)
     fila = novo_hyper
 else:
-    url = ja_lidos[-1]
     urls_lidas = open("{}\\todas_urls.txt".format(dir_path),"r", encoding="utf-16")
-    fila = urls_lidas.readlines() 
+    fila = urls_lidas.readlines()
+#    fila = set(fila)
     urls_lidas.close()
-urls_lidas = open("{}\\ja_lidos.txt".format(dir_path),"a+")
-#ja_lidos=[]   
-for i in range(7):
-#    print(i)
+#ja_lidos=[]
+tempo = datetime.now()
+print('start') 
+for i in range(100):
+    print(i)
     url = fila.pop(0)
-    if url in ja_lidos:
+    if url in ja_lidos or url[0:6] != '/wiki/':
         continue
     ja_lidos.append(url)
+    urls_lidas = open("{}\\ja_lidos.txt".format(dir_path),"a+")
     urls_lidas.write(url)
-    novo_hyper = webScrapper("https://pt.wikipedia.org"+url)
-    fila=fila+novo_hyper
-urls_lidas.close()
-
+    try:
+        novo_hyper = webScrapper("https://pt.wikipedia.org"+url)
+        fila=fila+novo_hyper
+    except:
+        pass
+    urls_lidas.close()
+print(datetime.now()-tempo)
